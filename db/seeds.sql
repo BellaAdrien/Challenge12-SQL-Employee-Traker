ffc3a9b6a1dcf003799808b264a3c2640c3653ea
BellaAdrien <Iadrien2020@fau.edu>
2023-09-13 20:14:54 -0400

    save12

--- db/seeds.sql
+++ db/seeds.sql
@@ -18,6 +18,15 @@ VALUES ("Sales Lead",100000,1),
 
 
 
+INSERT INTO employee (first_name,last_name,role_id)
+VALUES ("John","Doe",1),
+       ("Mike","Chan",2),
+       ("Ashley","Rodriguez",3),
+       ("Kevin","Tupik",4),
+       ("Kunal","Singh",5),
+       ("Malia","Brown",6),
+       ("Sarah","Lourd",7),
+       ("Tom","Allen",8);
 
 
 

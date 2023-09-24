3ab5b3c3db41b23f78baa57d0627b69262569082
BellaAdrien <Iadrien2020@fau.edu>
2023-09-24 16:37:44 -0400

    save12

--- index.js
+++ index.js
@@ -1,81 +1,91 @@
 const inquire = require("inquirer")
-const mysql= require("mysql2")
-const {printTable}=require("console-table-printer")
+const mysql = require("mysql2")
+const { printTable } = require("console-table-printer")
 
 require("dotenv").config()
 
-const db= mysql.createConnection(
-    {host: "localhost",
-    user:process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-port:3306
+const db = mysql.createConnection(
+    {
+        host: "localhost",
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        port: 3306
 
-}
+    }
 )
-db.connect(()=>{
-mainMenu()
+db.connect(() => {
+    mainMenu()
 
 })
 
-function mainMenu(){
-   inquirer.prompt({
+function mainMenu() {
+    inquirer.prompt({
 
-type:"list",
-message:"What would you like to do?",
-name:"selections",
-choices:["view all departments", "view all roles", "view all employees", "add a department", "add a role", "add an employee", "update an employee role"]
-   })
-   .then(answer=>{
-    if(answer.selection==="view all employees"){
-viewEmployees()
-    }else if(answer.selection==="add an employee"){
-addEmployee()
-    }else if(answer.selection==="update an employee role"){
-updateEmployeeRole()
-    }
-   }) 
+        type: "list",
+        message: "What would you like to do?",
+        name: "selections",
+        choices: ["view all departments", "view all roles", "view all employees", "add a department", "add a role", "add an employee", "update an employee role"]
+    })
+        .then(answer => {
+            if (answer.selection === "view all employees") {
+                viewEmployees()
+            } else if (answer.selection === "add an employee") {
+                addEmployee()
+            } else if (answer.selection === "update an employee role") {
+                updateEmployeeRole()
+            }
+        })
 
-   .then(answer=>{
-    if(answer.selection==="view all departments"){
-viewAllDepartments()
-    }else if(answer.selection==="add a department"){
-addADepartment()
-    }
-})
+        .then(answer => {
+            if (answer.selection === "view all departments") {
+                viewAllDepartments()
+            } else if (answer.selection === "add a department") {
+                addADepartment()
+            }
+        })
 
-.then(answer=>{
-    if(answer.selection==="view all roles"){
-viewAllRoles()
-    }else if(answer.selection==="add a role"){
-addARole()
-    }
-   }) 
-
-
-
-}
+        .then(answer => {
+            if (answer.selection === "view all roles") {
+                viewAllRoles()
+            } else if (answer.selection === "add a role") {
+                addARole()
+            }
+        })
 
 
-function viewEployees(){
-
-}
-function addEmployee(){
-
-}
-function updateEmployeeRole(){
-
-}
-function viewAllDepartments(){
 
 }
 
-function addADepartment(){
+
+function viewEployees() {
+    db.query(`SELECT employee.id, employee.first_name, employee.last_name, title, name as department, salary, 
+CONCAT( bosses.first_name, ' ',bosses.last_name) as manager 
+from employee
+left join role on employee.role_id = role.id
+left join department on department.id = role.department_id
+left join employee as bosses on employee.manager_id=bosses.id;
+`, (err, data) => {
+        printTable(data)
+        mainMenu()
+    })
+}
+function addEmployee() {
 
 }
-function viewAllRoles(){
+function updateEmployeeRole() {
 
 }
-function addARole(){
-    
+function viewAllDepartments() {
+
+}
+
+function addADepartment() {
+
+}
+function viewAllRoles() {
+
+}
+function addARole() {
+
 }
